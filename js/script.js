44a2b1b261c745e715a366b2c27799f997d7f224
Abbas <meerabbas110@gmail.com>
2023-06-25 17:54:40 +0530

    Swipper functionality by libreary added

--- js/script.js
+++ js/script.js
@@ -12,6 +12,56 @@ function highlightActiveLinks() {
     })
 }
 
+
+//! Display Slider Movies
+// Display Slider Movies
+async function displaySlider() {
+    const { results } = await fetchAPIData('movie/now_playing');
+  
+    results.forEach((movie) => {
+      const div = document.createElement('div');
+      div.classList.add('swiper-slide');
+  
+      div.innerHTML = `
+        <a href="movie-details.html?id=${movie.id}">
+          <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+        </a>
+        <h4 class="swiper-rating">
+          <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+        </h4>
+      `;
+  
+      document.querySelector('.swiper-wrapper').appendChild(div);
+  
+      initSwiper();
+    });
+  }
+  
+  function initSwiper() {
+    const swiper = new Swiper('.swiper', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      freeMode: true,
+      loop: true,
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+      breakpoints: {
+        500: {
+          slidesPerView: 2,
+        },
+        700: {
+          slidesPerView: 3,
+        },
+        1200: {
+          slidesPerView: 4,
+        },
+      },
+    });
+  }
+
+
 //! Display 20 most popular movies on home page
 async function displayPopularMovies() {
     const { results } = await fetchAPIData('movie/popular')
@@ -284,6 +334,7 @@ function init() {
     switch (global.currentpage) {
         case '/':
         case '/index.html': 
+            displaySlider()
             displayPopularMovies();
         break;
         case '/shows.html':
