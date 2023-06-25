6c225d7735a1ac451a3e9e0b44aa407248bbbf39
Abbas <meerabbas110@gmail.com>
2023-06-25 16:28:39 +0530

    Able to display all tv show with bit of details

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ function highlightActiveLinks() {
     })
 }
 
-//display 20 most popular movies on home page
+//! Display 20 most popular movies on home page
 async function displayPopularMovies() {
     const { results } = await fetchAPIData('movie/popular')
     
@@ -44,7 +44,7 @@ async function displayPopularMovies() {
     })
 }
 
-//display 20 most popular TV show
+//! Display 20 most popular TV show
 async function displayPopularShows() {
     const { results } = await fetchAPIData('tv/popular')
 
@@ -80,12 +80,12 @@ async function displayPopularShows() {
 }
 
 
-// Display Movie Details 
+//! Display Movie Details 
 async function displayMovieDetails() {
     const movieId = window.location.search.split('=')[1]
     
     const movie = await fetchAPIData(`movie/${movieId}`)
-    console.log(movie);
+
     //overlay for background image
     displayBackgroundImage('movie', movie.backdrop_path)
 
@@ -154,6 +154,73 @@ async function displayMovieDetails() {
 }
 
 
+//! Display TV Show Details 
+async function displayShwoDetails() {
+    const showId = window.location.search.split('=')[1]
+    
+    const show = await fetchAPIData(`tv/${showId}`)
+    
+    //overlay for background image
+    displayBackgroundImage('tv', show.backdrop_path)
+
+    const div = document.createElement('div')
+    
+    div.innerHTML = `
+    <div class="details-top">
+  <div>
+  ${
+    show.poster_path 
+    ? `<img 
+    src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+    class="card-img-top"
+    alt="${show.name}" 
+    />`
+    : `<img
+    src="images/no-image.jpg"
+    class="card-img-top"
+    alt="${show.name}"
+    />`
+  }
+  </div>
+  <div>
+    <h2>${show.name}</h2>
+    <p>
+      <i class="fas fa-star text-primary"></i>
+      ${show.vote_average.toFixed(1)} / 10
+    </p>
+    <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
+    <p>
+    ${show.overview}
+    </p>
+    <h5>Genres</h5>
+    <ul class="list-group">
+    ${show.genres.map((genre) => `<li>${genre.name}</li>`)
+    .join('')}
+    </ul>
+    <a href="${
+        show.homepage
+    }" target="_blank" class="btn">Visit Movie Homepage</a>
+  </div>
+</div>
+<div class="details-bottom">
+  <h2>Show Info</h2>
+  <ul>
+    <li><span class="text-secondary">Number of Episodes:</span> ${show.number_of_episodes}
+    </li>
+    <li><span class="text-secondary">Last Episode To Air:</span> ${show.last_episode_to_air}
+    </li>
+    <li><span class="text-secondary">Status:</span> 
+    ${show.status}</li>
+  </ul>
+  <h4>Production Companies</h4>
+  <div class="list-group">
+  ${show.production_companies
+    .map((company) => `<span>${company.name}</span>`).join('')}
+  </div>
+</div>`;
+
+   document.querySelector('#show-details').appendChild(div)
+}
 
 
 
@@ -226,7 +293,7 @@ function init() {
             displayMovieDetails();
         break;
         case '/tv-details.html':
-            console.log('tv details');
+            displayShwoDetails()
         break;
         case '/search.html':
             console.log('search');
